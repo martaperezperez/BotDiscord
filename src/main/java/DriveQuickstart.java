96401abfa81bb3ee911a606cf026e0cbc18ec402
1 <mperezperez@danielcastelao.org>
2022-06-03 09:16:31 +0200

    Rama feature_pdf para que te descarge un pdf

--- src/main/java/DriveQuickstart.java
+++ src/main/java/DriveQuickstart.java
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 // [START drive_quickstart]
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -29,25 +30,35 @@ import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
 
 import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.time.Instant;
+
+
 
 /* class to demonstarte use of Drive files list API */
 public class DriveQuickstart {
-    /** Application name. */
+    /**
+     * Application name.
+     */
     private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
-    /** Global instance of the JSON factory. */
+    /**
+     * Global instance of the JSON factory.
+     */
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    /** Directory to store authorization tokens for this application. */
+    /**
+     * Directory to store authorization tokens for this application.
+     */
     private static final String TOKENS_DIRECTORY_PATH = "resources";
 
     /**
      * Global instance of the scopes required by this quickstart.
      * If modifying these scopes, delete your previously saved tokens/ folder.
-     *
+     * <p>
      * En el ejemplo original esta readonly metadatos, por lo tanto si lo dejamos asi
      * no podremos descargar ficheros, solo listarlos
      */
@@ -56,6 +67,7 @@ public class DriveQuickstart {
 
     /**
      * Creates an authorized Credential object.
+     *
      * @param HTTP_TRANSPORT The network HTTP Transport.
      * @return An authorized Credential object.
      * @throws IOException If the credentials.json file cannot be found.
@@ -80,48 +92,53 @@ public class DriveQuickstart {
         return credential;
     }
 
-    public static void main(String... args) throws IOException, GeneralSecurityException {
-        // Build a new authorized API client service.
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+    public class Bot {
+        public void main(String... args) throws IOException, GeneralSecurityException {
+            // Build a new authorized API client service.
+            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                    .setApplicationName(APPLICATION_NAME)
+                    .build();
 
-        // Filtra para encontrar la carpeta que se llama imagenesBot
-        FileList result = service.files().list()
-                .setQ("name contains 'imagenes' and mimeType = 'application/vnd.google-apps.folder'")
-                .setPageSize(100)
-                .setSpaces("drive")
-                .setFields("nextPageToken, files(id, name)")
-                .execute();
-        List<File> files = result.getFiles();
-
-        if (files == null || files.isEmpty()) {
-            System.out.println("No files found.");
-        } else {
-            String dirImagenes = null;
-            System.out.println("Files:");
-            for (File file : files) {
-                System.out.printf("%s (%s)\n", file.getName(), file.getId());
-                dirImagenes = file.getId();
-            }
-            // busco la imagen en el directorio
-            FileList resultImagenes = service.files().list()
-                    .setQ("name contains 'imagen' and parents in '"+dirImagenes+"'")
+            // Filtra para encontrar la carpeta que se llama examen
+            FileList result = service.files().list()
+                    .setQ("name contains 'examen' and mimeType = 'application/vnd.google-apps.folder'")
+                    .setPageSize(100)
                     .setSpaces("drive")
                     .setFields("nextPageToken, files(id, name)")
                     .execute();
-            List<File> filesImagenes = resultImagenes.getFiles();
-            for (File file : filesImagenes) {
-                System.out.printf("imagen: %s\n", file.getName());
-                // guardamos el 'stream' en el fichero aux.jpeg qieune qe existir
-                OutputStream outputStream = new FileOutputStream("C:\\Users\\Interno\\IdeaProjects\\BotDiscord\\src\\main\\java\\imagenes\\imagen.jpg");
-                service.files().get(file.getId())
-                        .executeMediaAndDownloadTo(outputStream);
-                outputStream.flush();
-                outputStream.close();
+            List<File> files = result.getFiles();
+
+            if("/pdf".equals(result.getFiles())) {
+                String dirPdf = null;
+                System.out.println("Files:");
+                for (File file : files) {
+                    System.out.printf("%s (%s)\n", file.getName(), file.getId());
+                    dirPdf = file.getId();
+            }
+
+                // busco el archivo  en el directorio
+                FileList resultPdf = service.files().list()
+                        .setQ("name contains 'examen' and parents in '" + dirPdf + "'")
+                        .setSpaces("drive")
+                        .setFields("nextPageToken, files(id, name)")
+                        .execute();
+                List<File> filesPdf = resultPdf.getFiles();
+                for (File file : filesPdf) {
+                    System.out.printf("examen: %s\n", file.getName());
+                    // guardamos el 'stream' en el fichero examen.pdf tiene que existir
+                    OutputStream outputStream = new FileOutputStream("C:\\Users\\Interno\\Documents\\examen.pdf");
+                    service.files().get(file.getId())
+                            .executeMediaAndDownloadTo(outputStream);
+                    outputStream.flush();
+                    outputStream.close();
+                }
+
+            }
+            else{
+                System.out.println("No files found.");
             }
         }
     }
-}
+    }
 // [END drive_quickstart]
